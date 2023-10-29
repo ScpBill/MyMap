e2a6c6778558035ed15bfea3d4cc9565a2d25283
Rinny <artpr.zxc@gmail.com>
2023-10-29 12:47:21 +0500

    unification: _value and _keys arrays to the _entries

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 class MyMap {
     constructor(iterable = null) {
-        this._keys = [];
-        this._values = [];
+        this._entries = [];
 
         if (iterable === null || iterable === undefined) return;
 
@@ -14,9 +13,12 @@ class MyMap {
                 let nameOfObject = entry.toString();
                 throw new TypeError(`Iterator value ${nameOfObject} is not an entry object`)
             }
-            let indexEntry = this._keys.indexOf(entry[0]);
-            indexEntry === -1 ? this._keys.push(entry[0]) : this._keys[indexEntry] = entry[0]
-            indexEntry === -1 ? this._values.push(entry[1]) : this._values[indexEntry] = entry[1]
+            let indexEntry = this._entries.indexOf(entry[0]);
+            if (indexEntry === -1) {
+                this._entries.push([entry[0], entry[1]]);
+            } else {
+                this._entries[indexEntry][1] = entry[1];
+            }
         }
     }
     static groupBy(items, callbackFn) {
@@ -28,7 +30,7 @@ class MyMap {
         return mapObj;
     }
     get size() {
-        return this._keys.length;
+        return this._entries.length;
     }
     set size(value) {
         return value;
@@ -37,14 +39,12 @@ class MyMap {
         return 'MyMap';
     }
     clear() {
-        this._keys = [];
-        this._values = [];
+        this._entries = [];
     }
     delete(key) {
-        let indexKey = this._keys.indexOf(key);
+        let indexKey = this._entries.findIndex(x => x[0] == key);
         if (indexKey !== -1) {
-            this._keys.splice(indexKey, 1);
-            this._values.splice(indexKey, 1);
+            this._entries.splice(indexKey, 1);
             return true;
         } else {
             return false;
@@ -52,53 +52,54 @@ class MyMap {
     }
     entries() {
         let index = 0;
-        let entries = Array.from(
-            {length: this._keys.length},
-            (_, i) => [this._keys[i], this._values[i]]
-        )
         return {
             next: () => ({
-                value: index >= this._keys.length ? undefined : entries[index],
-                done: index++ >= this._keys.length
+                value: index >= this._entries.length ? undefined : this._entries[index],
+                done: index++ >= this._entries.length
             })
         };
     }
     forEach(callbackFn, thisArg = null) {
-        for (let i = 0; i < this._keys.length; i++) {
-            callbackFn.call(thisArg || window, this._values[i], this._keys[i], this);
+        for (let i = 0; i < this._entries.length; i++) {
+            callbackFn.call(thisArg || globalThis, this._entries[i][1], this._entries[i][0], this);
         }
     }
     get(key) {
-        let indexKey = this._keys.indexOf(key);
-        return indexKey === -1 ? undefined : this._values[indexKey];
+        let indexKey = this._entries.findIndex(x => x[0] == key);
+        return indexKey === -1 ? undefined : this._entries[indexKey][1];
     }
     has(key) {
-        return this._keys.includes(key);
+        return this._entries.some(x => x[0] == key);
     }
     keys() {
         let index = 0;
         return {
+            [Symbol.iterator]() {
+                return this;
+            },
             next: () => ({
-                value: index >= this._keys.length ? undefined : this._keys[index],
-                done: index++ >= this._keys.length
+                value: index >= this._entries.length ? undefined : this._entries[index][0],
+                done: index++ >= this._entries.length
             })
         };
     }
     set(key, value) {
-        let indexKey = this._keys.indexOf(key);
+        let indexKey = this._entries.findIndex(x => x[0] == key);
         if (indexKey !== -1) {
-            this._values[indexKey] = value;
+            this._entries[indexKey][1] = value;
         } else {
-            this._keys.push(key);
-            this._values.push(value);
+            this._entries.push([key, value]);
         }
     }
     values() {
         let index = 0;
         return {
+            [Symbol.iterator]() {
+                return this;
+            },
             next: () => ({
-                value: index >= this._keys.length ? undefined : this._values[index],
-                done: index++ >= this._keys.length
+                value: index >= this._entries.length ? undefined : this._entries[index][1],
+                done: index++ >= this._entries.length
             })
         };
     }
@@ -106,7 +107,6 @@ class MyMap {
         return this.entries();
     }
 }
-
 
 
 let recipeMap = new MyMap([
